cc2c7a662d35896a98f7ce91c62beb70ecc36792
illguthp <illguthphillip@yahoo.com>
2025-06-18 18:42:09 -0600

    changes to exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,7 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("What is the magic number? ");
+        //Part 1 and part 2
+        //string magicNumber = Console.ReadLine();
+        //int number = int.Parse(magicNumber);
+
+        // Using this integer to start the while loop
         int var = 8;
+
+        //Part 3
+
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 100);
         while (var == 8)
