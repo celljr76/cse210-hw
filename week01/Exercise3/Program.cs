429a2b7b45786c856d782b30993aff4c3831924d
illguthp <illguthphillip@yahoo.com>
2025-06-18 18:36:20 -0600

    Week 1 Exercise 3 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int var = 8;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        while (var == 8)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int guessNumber = int.Parse(guess);
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber == magicNumber)
+            {
+                var = 9;
+            }
+
+        }
+        Console.WriteLine("You guessed it!");
+        
     }
 }
