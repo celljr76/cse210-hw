24825f546cf7abcb1bebd8762eab6a293689ee02
illguthp <illguthphillip@yahoo.com>
2025-06-18 21:35:58 -0600

    Week 1 Exercise 4 complete

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using System.Transactions;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int number = -1;
+        
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        }
+        int total = numbers.Sum();
+        Console.WriteLine($"The sum is : {total}");
+        int listCount = numbers.Count;
+        Console.WriteLine($"The average is: {total / listCount}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
