b88e412a1339825782ad6e23e6e7103b45ad653a
illguthp <illguthphillip@yahoo.com>
2025-06-26 21:53:53 -0600

    I forgot to commit this

--- week02/Journal/Prompts.cs
+++ week02/Journal/Prompts.cs
@@ -1,11 +1,9 @@
 public class Prompts
 {
     public List<string> _prompts;
-
-
     public string GetRandomPrompt()
     {
-        //_prompts.Add("What was the best part of your day?");
+        _prompts.Add("What was the best part of your day?");
         _prompts.Add("Did you do any good today?");
         _prompts.Add("Did you have a meaningful conversation with anyone?");
         _prompts.Add("Did you talk to your parents today?");
@@ -16,7 +14,7 @@ public class Prompts
         int randomPrompt = randomgenerator.Next(1, _prompts.Count + 1);
         string _prompt = _prompts[randomPrompt];
         return _prompt;
-    
+
     }
 
     
