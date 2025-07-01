97446318b0f265c0e0d8c2aadc62af0cf8fcae48
illguthp <illguthphillip@yahoo.com>
2025-07-01 13:35:04 -0600

    week 3, added into the reference class

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,11 +1,12 @@
 public class Reference
 {
-    public string _book = "";
-    public int _chapter;
-    public int _verse;
-    public int _endverse;
+    public string _book = "John";
+    public int _chapter = 3;
+    public int _verse = 5;
+    public int _endverse = 6;
     public string GetDisplayText()
     {
         return "";
     }
+    
 }
