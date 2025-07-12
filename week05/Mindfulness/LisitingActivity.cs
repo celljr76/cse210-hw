5f6a13c69ebb269eb6f017836ec52e78ddce8fc0
illguthp <illguthphillip@yahoo.com>
2025-07-11 22:00:11 -0600

    outline of Week 5 assignment

---
+++ week05/Mindfulness/LisitingActivity.cs
@@ -0,0 +1,27 @@
+public class ListingActivity
+{
+
+    public int _count;
+    public List<string> _prompts;
+
+    public ListingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        List<string> _userList = new List<string> { };
+        return _userList;
+    }
+}
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,36 @@
+public class ReflectingActivity
+{
+
+    public List<string> _prompts;
+    public List<string> _questions;
+    public ReflectingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "";
+
+    }
+
+    public void DisplayPrompt()
+    {
+        
+    }
+
+    public void DisplayQuestions()
+    {
+        
+    }
+}
